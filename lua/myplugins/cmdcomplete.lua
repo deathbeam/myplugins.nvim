416c86d8a75427e5adc3105a772c5f697f439ab7
Tomas Slusny <slusnucky@gmail.com>
2025-08-07 19:32:38 +0200

    refactor(cmdcomplete): simplify command line completion trigger
    Replaces manual wildcharm and feedkeys logic with vim.fn.wildtrigger().
    Removes unnecessary configuration and streamlines the setup for command
    line completion. This improves maintainability and reduces complexity.

--- lua/myplugins/cmdcomplete.lua
+++ lua/myplugins/cmdcomplete.lua
@@ -6,12 +6,8 @@ function M.setup()
     end
 
     local group = vim.api.nvim_create_augroup('myplugins-cmdcomplete', { clear = true })
-    local term = vim.api.nvim_replace_termcodes('<C-@>', true, true, true)
 
-    vim.cmd([[set wildcharm=<C-@>]])
-    vim.opt.wildmenu = true
     vim.opt.wildmode = 'noselect:lastused,full'
-
     vim.keymap.set('c', '<Up>', '<End><C-U><Up>', { silent = true })
     vim.keymap.set('c', '<Down>', '<End><C-U><Down>', { silent = true })
 
@@ -20,23 +16,7 @@ function M.setup()
         desc = 'Auto show command line completion',
         pattern = ':',
         callback = function()
-            local cmdline = vim.fn.getcmdline()
-            local curpos = vim.fn.getcmdpos()
-            local last_char = cmdline:sub(curpos - 1, curpos - 1)
-
-            if
-                curpos == #cmdline + 1
-                and vim.fn.pumvisible() == 0
-                and last_char:match('[%w%/%:- ]')
-                and not cmdline:match('^%d+$')
-            then
-                vim.api.nvim_feedkeys(term, 'ti', false)
-                vim.opt.eventignore:append('CmdlineChanged')
-                vim.schedule(function()
-                    vim.fn.setcmdline(vim.fn.substitute(vim.fn.getcmdline(), '\\%x00', '', 'g'))
-                    vim.opt.eventignore:remove('CmdlineChanged')
-                end)
-            end
+            vim.fn.wildtrigger()
         end,
     })
 end
